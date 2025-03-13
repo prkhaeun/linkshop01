8a79181e877b8578063b6adebffea593121e33a1
prkhaeun <hani00415@naver.com>
2025-03-13 14:31:15 +0900

    Create Top.jsx

---
+++ src/Top.jsx
@@ -0,0 +1,9 @@
+import styles from "./Top.module.css";
+
+export const Top = () => {
+  return (
+    <div className={styles.topContainer}>
+      <img src="Property 1=Variant3.png" alt="Top 이미지" className={styles.topImage} />
+    </div>
+  );
+};
